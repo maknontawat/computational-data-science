51a4592e60a0dc1ec4db995571ca498515410cd1
anaveed <ahsan_naveed@sfu.ca>
2019-06-06 16:03:30 -0700

    Uses np.divide()

--- e4/temperature_correlation.py
+++ e4/temperature_correlation.py
@@ -15,8 +15,8 @@ def get_cities_data(file):
     """
 
     data = pd.read_csv(file).dropna(subset=['area', 'population'])
-    data['area'] = data['area'] / 1000000
-    data['density'] = data['population']/data['area']
+    data['area'] = np.divide(data['area'],  1000000)
+    data['density'] = np.divide(data['population'], data['area'])
     data = data[data['area'] <= 10000]
 
     return data
